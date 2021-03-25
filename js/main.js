3bf88e4e25fbf0917136dcd1cece2817b4be4898
Numbat <mercantile.code@gmail.com>
2021-03-25 13:05:14 +0000

    👨‍❤️‍👨🐪 Checkpoint ./js/main.js:30080173/242

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   });
 });
 function getSelection() {
-  var valueSelected = $("#choice1").val();
-  document.getElementById("choice1Out").innerHTML = valueSelected;
+  var value1Selected = $("#choice1").val();
+  var value2Selected = $("#choice2").val();
+  var value3Selected = $("#choice3").val();
+  document.getElementById("choice1Out").innerHTML = value1Selected;
+  document.getElementById("choice2Out").innerHTML = value2Selected;
+  document.getElementById("choice3Out").innerHTML = value3Selected;
 }
